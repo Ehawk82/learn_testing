5ac07c3a5ce858d54fe171758c8150165099426a
Jason Graziano <mrehawk82@gmail.com>
2020-11-14 19:08:09 -0500

    rework

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function seq(requested_length) { for (var i = 0; i < requested_length; i++) {
-	let myArray = [];
-	myArray.push(i);
-}};
+let myArray = [];
+function seq(requested_length) { 
+	for (var i = 0; i < requested_length; i++) {
+		myArray.push(i);
+		if (i === requested_length - 1) {
+			console.log(myArray);
+		}
+	}
+}
 
 seq(7);
