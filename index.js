b16e09ed6f1ac79248758bd0323b3be0ee9a7899
Jason Graziano <mrehawk82@gmail.com>
2020-11-14 19:09:58 -0500

    reformatting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let myArray = [];
 function seq(requested_length) { 
-	for (var i = 0; i < requested_length; i++) {
-		myArray.push(i);
-		if (i === requested_length - 1) {
-			console.log(myArray);
-		}
-	}
+  for (var i = 0; i < requested_length; i++) {
+    myArray.push(i);
+    if (i === requested_length - 1) { // spit out the final array only once at completion
+    	console.log(myArray);
+    }
+  }
 }
 
 seq(7);
